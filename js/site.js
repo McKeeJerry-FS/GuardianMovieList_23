ebc63c672d9e8e16ef9514d6b2fde4c007370a01
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2023-10-17 15:24:15 -0400

    updated favicon

--- js/site.js
+++ js/site.js
@@ -1,10 +1,1 @@
-function displayMessage() {
-  // get the text out of input and assign to a variable
-  let msg = document.getElementById("message").value;
 
-  Swal.fire({
-    backdrop: false,
-    title: "MovieLog",
-    text: msg, // <-- replace this with the variable
-  });
-}
