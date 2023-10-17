1a7d49803e2f992732934fec69897956926361e2
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2023-10-17 15:03:16 -0400

    Initial commit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,10 @@
 function displayMessage() {
-    // get the text out of input and assign to a variable
-    let msg = document.getElementById('message').value;
-    
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this with the variable
-        }
-    );
+  // get the text out of input and assign to a variable
+  let msg = document.getElementById("message").value;
+
+  Swal.fire({
+    backdrop: false,
+    title: "MovieLog",
+    text: msg, // <-- replace this with the variable
+  });
 }
